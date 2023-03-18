8552d7004c2fef4a16162208af3b20d5a85a80fa
Karen-Shekyan <kshekyan30@stuy.edu>
2023-03-18 04:01:38 -0400

    BIG cleanup

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, session, request, redirect,
+import requests, os, json
 
-def create_app():
-  app = Flask(__name__)
-  app.config["key"] = "google"
-  
-  from .views import views
-  from .auth import auth
+app = Flask(__name__)
 
-  # used to create prefixes for the different webpages
-  app.register_blueprint(views, url_prefix='/')
-  app.register_blueprint(auth, url_prefix='/')
+@app.route('/')
+def log_in():
+    if 'username' in session:
+        return redirect('/home')
+    return render_template('login.html') #For login AND signup
 
-  return app
+@app.route('/home')
+def home():
+    if (session):
+        return render_template('home.html', user = 'username')
+    else:
+        return redirect('/')
+
+# def create_app():
+#   app = Flask(__name__)
+#   app.config["key"] = "google" what is this???
+#
+#   from .views import views
+#   from .auth import auth
+#
+#   # used to create prefixes for the different webpages
+#   # app.register_blueprint(views, url_prefix='/')
+#   # app.register_blueprint(auth, url_prefix='/')
+#
+#   return app
+
+if __name__ == "__main__": #used to execute the file
+  app.run(debug=True)
