82d27f2458978afb82ce61e0110648ef2c5d56d7
Karen-Shekyan <kshekyan30@stuy.edu>
2023-03-18 04:03:10 -0400

    comments

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def log_in():
-    if 'username' in session:
+    if 'username' in session: # If already logged in
         return redirect('/home')
-    return render_template('login.html') #For login AND signup
+    return render_template('login.html') # For login AND signup
 
 @app.route('/home')
 def home():
-    if (session):
+    if (session): # If logged in, show the home page
         return render_template('home.html', user = 'username')
-    else:
+    else: # ...else show the login page
         return redirect('/')
 
 # def create_app():
 #   app = Flask(__name__)
-#   app.config["key"] = "google" what is this???
+#   app.config["key"] = "google" #what is this???
 #
 #   from .views import views
 #   from .auth import auth
